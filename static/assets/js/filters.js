2fdbe9a6c9d401df68ba9c5ab2d14def79c9e1bb
Rosemary Ferrier <rosemary.ferrier@gmail.com>
2023-07-03 11:43:43 +0100

    simplify js by toggling classes instead & aria-live the changing area

--- static/assets/js/filters.js
+++ static/assets/js/filters.js
@@ -1,32 +1,9 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  const themeButton = document.querySelector(".work__types-btn-theme");
-  themeButton.style.color = "#f1f4d1";
-  themeButton.style.backgroundColor = "#ea5b0c";
-});
+const showFilters = () => {
+  document
+    .querySelectorAll(".work__types-btn")
+    .forEach((button) => button.classList.toggle("work__btn--active"));
 
-const showFilters = (type) => {
-  const themeElements = Array.from(
-    document.getElementsByClassName("work__theme")
-  );
-  const categoryElements = Array.from(
-    document.getElementsByClassName("work__category")
-  );
-  const themeButton = document.querySelector(".work__types-btn-theme");
-  const categoryButton = document.querySelector(".work__types-btn-category");
-
-  if (type === "theme") {
-    themeElements.map((e) => (e.style.display = "unset"));
-    categoryElements.map((e) => (e.style.display = "none"));
-    themeButton.style.color = "#f1f4d1";
-    themeButton.style.backgroundColor = "#ea5b0c";
-    categoryButton.style.color = "";
-    categoryButton.style.backgroundColor = "";
-  } else {
-    themeElements.map((e) => (e.style.display = "none"));
-    categoryElements.map((e) => (e.style.display = "unset"));
-    categoryButton.style.color = "#f1f4d1";
-    categoryButton.style.backgroundColor = "#ea5b0c";
-    themeButton.style.color = "";
-    themeButton.style.backgroundColor = "";
-  }
+  document
+    .querySelectorAll(".work__filter")
+    .forEach((button) => button.classList.toggle("work__filter--hidden"));
 };
